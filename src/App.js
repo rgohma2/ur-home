afdabcecc36d56c5c19b8a0dd5c1843acc64c39d
Randy Gohmann <rgohma2@gmail.com>
2020-11-14 16:46:49 -0600

    added pagination and rendering ComponentsContainer

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import ComponentsContainer from './ComponentsContainer'
 import './App.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -5,37 +7,56 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
+import Fade from 'react-bootstrap/Fade';
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar bg="light" className="justify-content-between">
-        <Nav>
-        <Navbar.Brand href="#home">
-          U R Home
-        </Navbar.Brand>
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#about_us">About Us</Nav.Link>
-          <NavDropdown title="Services" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#services/shopping_needs">Shopping Needs</NavDropdown.Item>
-            <NavDropdown.Item href="#services/transportation">Transportation</NavDropdown.Item>
-            <NavDropdown.Item href="#services/luggage_storage">Luggage Storage</NavDropdown.Item>
-            <NavDropdown.Item href="#services/tours_&_special_events">Tours & Special Events</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Nav>
-          <Nav.Link href="#contact">Contact</Nav.Link>
-        </Nav>
-      </Navbar>
-      <Image 
-      src="/ur-home-background.jpg" 
-      width="100%"
-      /> 
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      page: 'home',
+    }
+
+  }
+
+  handleChange = (e) => {
+    e.preventDefault()
+    this.setState({page: e.target.accessKey})
+  }
+
+
+
+
+
+  render (){
+    return (
+      <div className="App">
+        <Navbar bg="light" className="justify-content-between" sticky="top">
+          <Nav>
+          <Navbar.Brand onClick={this.handleChange} accessKey={'home'} href="home">
+            U R Home
+          </Navbar.Brand>
+            <Nav.Link onClick={this.handleChange} accessKey={'home'} href="home">Home</Nav.Link>
+            <Nav.Link onClick={this.handleChange} accessKey={'about'} href="about_us">About Us</Nav.Link>
+            <NavDropdown title="Services" id="basic-nav-dropdown">
+              <NavDropdown.Item onClick={this.handleChange} accessKey={'shopping'} href="services/shopping_needs">Shopping Needs</NavDropdown.Item>
+              <NavDropdown.Item onClick={this.handleChange} accessKey={'transportation'} href="services/transportation">Transportation</NavDropdown.Item>
+              <NavDropdown.Item onClick={this.handleChange} accessKey={'luggage'} href="services/luggage_storage">Luggage Storage</NavDropdown.Item>
+              <NavDropdown.Item onClick={this.handleChange} accessKey={'tours'} href="services/tours_&_special_events">Tours & Special Events</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link onClick={this.handleChange} accessKey={'contact'} href="contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar>
+        <ComponentsContainer
+        page={this.state.page}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/ComponentsContainer/index.js
@@ -0,0 +1,150 @@
+import React from 'react'
+import Image from 'react-bootstrap/Image';
+
+class ComponentsContainer extends React.Component {
+	constructor(props) {
+
+		super(props)
+
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return(
+			<div>
+				{
+					this.props.page === 'home'
+					?
+					<div style={{backgroundColor: '#dce1e6', height:"8000px"}}>
+			          <Image 
+			          src="/ur-home-background.jpg" 
+			          width="100%"
+			          />
+			            <h1 
+			            id='fade-text'
+			            style={{
+			              padding: '20px',
+			              color: '#242c33'
+			            }}
+			            >
+			              Let us make your Savannah vacation stay <span style={{}}>remarkable</span>
+			            </h1>
+			        </div>
+			        :
+			        null
+				}
+				{
+					this.props.page === 'about'
+					?
+					<div style={{backgroundColor: '#dce1e6', height:"8000px"}}>
+			          <Image 
+			          src="/ur-home-about.jpeg" 
+			          width="80%"
+			          />
+			            <h1 
+			            id='fade-text'
+			            style={{
+			              padding: '20px',
+			              color: '#242c33'
+			            }}
+			            >
+			              About Us
+			            </h1>
+			        </div>
+			        :
+			        null
+				}
+				{
+					this.props.page === 'shopping'
+					?
+					<div style={{backgroundColor: '#dce1e6', height:"8000px"}}>
+			          <Image 
+			          src="/ur-home-shopping.jpeg" 
+			          width="70%"
+			          />
+			            <h1 
+			            id='fade-text'
+			            style={{
+			              padding: '20px',
+			              color: '#242c33'
+			            }}
+			            >
+			              Shopping Needs
+			            </h1>
+			        </div>
+			        :
+			        null
+				}
+				{
+					this.props.page === 'transportation'
+					?
+					<div style={{backgroundColor: '#dce1e6', height:"8000px"}}>
+			          <Image 
+			          src="/ur-home-transportation.jpg"
+			          width="70%" 
+			          
+			          />
+			            <h1 
+			            id='fade-text'
+			            style={{
+			              padding: '20px',
+			              color: '#242c33'
+			            }}
+			            >
+			              Transportation
+			            </h1>
+			        </div>
+			        :
+			        null
+				}
+				{
+					this.props.page === 'luggage'
+					?
+					<div style={{backgroundColor: '#dce1e6', height:"8000px"}}>
+			          <Image 
+			          src="/ur-home-luggage.jpg" 
+			          />
+			            <h1 
+			            id='fade-text'
+			            style={{
+			              padding: '20px',
+			              color: '#242c33'
+			            }}
+			            >
+			              Luggage Storage
+			            </h1>
+			        </div>
+			        :
+			        null
+				}
+				{
+					this.props.page === 'tours'
+					?
+					<div style={{backgroundColor: '#dce1e6', height:"8000px"}}>
+			          <Image 
+			          src="/ur-home-tours.jpg" 
+			          
+			          />
+			            <h1 
+			            id='fade-text'
+			            style={{
+			              padding: '20px',
+			              color: '#242c33'
+			            }}
+			            >
+			              Tours & Special Events
+			            </h1>
+			        </div>
+			        :
+			        null
+				}
+
+
+	        </div>
+		)
+	}
+}
+
+export default ComponentsContainer
